3661a4991df822c6076a201d7813cd81f9d8ba39
killian1 <3141270268@qq.com>
2024-09-22 01:56:17 +0800

    温度换算

---
+++ unit5_8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void Temperature(double f);
+int main()
+{
+    double t;
+    printf("Please enter a temperature in Fahrenheit:");
+    while(scanf("%lf",&t) == 1)
+    {
+        Temperature(t);
+        printf("Enter next temperature in Fahrenheit(or enter 'q' to quit):");
+    }
+}
+void Temperature(double f)
+{
+    double c,k;
+    const double r=32.0;
+    const double y=273.15;
+    c=5.0/9.0*(f-r);
+    k=c+y;
+    printf("The temperature in Celsius is %.3lf\n",c);
+    printf("The temperature in Kelvin is %.3lf\n",k);
+}
