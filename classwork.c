75480fd11c03412204ec66e72d76ea4060150075
Strateg1st <3141270268@qq.com>
2024-09-23 11:03:01 +0800

    更新 classwork.c

--- classwork.c
+++ classwork.c
@@ -4,6 +4,6 @@ int main()
     float x,y,z,sum;
     scanf("%f %f %f",&x,&y,&z);
     sum=x+y+z;
-    printf("The sum of three number is %g",sum);
+    printf("The sum of three number is %g\n",sum);
     return 0;
 }
