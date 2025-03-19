cc60b775378497dd924bd5bea4232e94582b770a
Zhiyuan <3141270268@qq.com>
2025-03-19 10:48:43 +0800

    classwork

--- classwork/include/user.h
+++ classwork/include/user.h
@@ -2,7 +2,7 @@
 #define USER_H
 #include <stdbool.h>
 
-#define MAX_USERS 10
+#define MAX_USERS 100
 #define MAX_NAME_LEN 32
 #define MAX_PASS_LEN 16
 #define MIN_PASS_LEN 6
@@ -11,8 +11,8 @@
 #define USERS_DEFINED
 
 typedef struct User {
-    char id[MAX_NAME_LEN];
-    char password[MAX_PASS_LEN];
+    char id[MAX_NAME_LEN + 1];
+    char password[MAX_PASS_LEN + 1];
     struct TreeNode* library;
 } user;
 
@@ -21,9 +21,14 @@ typedef struct User {
 extern user users[MAX_USERS];
 extern int usercount;
 
-int findUser(const char* username);
-int registerUser(user* u);
+int GetUsers(const char* filename, user* usersArray, int* usercount);
+int findUser(const char* username,
+             user* usersArray,
+             int userCount,
+             const char* filename);
+int registerUser(user* u, int* usercount, user* users);
 bool loginUser(user* u);
 int passwordReset(user* u);
+void saveUsersToFile(const char* filename, user* users, int usercount);
 
 #endif
