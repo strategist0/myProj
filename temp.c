4cdad2159440d12795efa66f4dabac5b32af6b65
Zhiyuan <3141270268@qq.com>
2025-03-21 22:40:29 +0800

    change

--- temp.c
+++ temp.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello World!\n");
+int main() {
+    double exp = 0.00000001;
+    double a, b;
+    scanf("%lf %lf", &a, &b);
+    printf("%lf/%lf = %lf\n", a, b, a / b);
+    getchar();
     system("pause");
     return 0;
 }
