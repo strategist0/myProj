2c1480c9f2e4472ef0ec9d46c4d276d6bda95309
Zhiyuan <3141270268@qq.com>
2025-04-20 15:10:07 +0800

    更新 temp.c

--- temp.c
+++ temp.c
@@ -4,9 +4,14 @@
 int main() {
     double exp = 0.00000001;
     double a, b;
+    
     scanf("%lf %lf", &a, &b);
+    
     printf("%lf/%lf = %lf\n", a, b, a / b);
+    
     getchar();
+    
     system("pause");
+    
     return 0;
 }
